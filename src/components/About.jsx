bbf881bf575740253561b965a3f14e0cd36f7cbb
Alvaro Martinez <alvaromfdv@gmail.com>
2023-08-03 17:25:21 +0200

    mvp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <h3>Hi, There</h3>
     <p>Im a curious and enthusiastic mind who finds true joy in the process of programming, innovation, and bringing new ideas to life.</p>
     <p>From a young age, I discovered the fascinating world of technology and its ability to shape our environment in infinite ways. What started as curiosity has become into a genuine passion.</p>
-    <p>Being creative is an integral part of my essence. Not only in the world of programming but I also find inspiration in the wonderful world of music and arts wich helps me sparks my imagination and serves as an additional source of motivation.</p>
+    <p>Being creative is an integral part of my essence. Not only in the world of programming but I also find inspiration in the wonderful world of music and arts wich helps me spark my imagination and serves as an additional source of motivation.</p>
     </div>
     <img src="/23022023-DOM04086-fotor-2023062393343.png" alt="profile" width={"200px"} className='profileImg'/>
     </div>
